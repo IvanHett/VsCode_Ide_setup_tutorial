270d11236b2399e224fcb391b4b921d7f35bf388
IvanHett <ivanhett.contact@gmail.com>
2026-04-08 14:42:31 +0100

    updated code 01_env create

--- 01_py_env_create.py
+++ 01_py_env_create.py
@@ -5,11 +5,18 @@ print("python is working normally")
 
 #02. Activate virtual environment
     # venv\Scripts\activate
+    # if path shows as (.venv) then it is activated
 
-#03. Check
+#03. CheckC:\Users\Ivant\AppData\Local\Programs\Microsoft VS Code
     # Ctrl + Shift + P → Python: Select Interpreter
 
 #04. Check pip version
+    # pip --version
+    # or  python -m pip --version
 
+#05. check whats installed
+    # pip list
+
+    
 #Install NumPy, Pandas, Matplotlib together
     # open terminal and type "pip install numpy pandas matplotlib"
